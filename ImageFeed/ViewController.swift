6ea9c87c1ecd25d1c83e826b3430fea856a8a370
id-kuznetsov <ilua.kuznetsov@gmail.com>
2024-08-29 16:54:33 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Илья Кузнецов on 29.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
