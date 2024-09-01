38a31e8dcb949d76ff0c67c0a1e413848a6f5799
id-kuznetsov <ilua.kuznetsov@gmail.com>
2024-09-01 19:51:21 +0300

    Создан экран профиля

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Илья Кузнецов on 01.09.2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
