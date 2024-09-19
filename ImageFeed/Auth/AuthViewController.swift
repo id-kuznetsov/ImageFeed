5a632c7adb70f8a77931a030a85b6b82936a74e6
id-kuznetsov <ilua.kuznetsov@gmail.com>
2024-09-19 23:17:41 +0300

    Создан и сверстан AuthViewController

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Илья Кузнецов on 19.09.2024.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
