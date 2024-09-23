2c0aacc6be320c2e50d7e3f3b3cce2e9843aaf00
id-kuznetsov <ilua.kuznetsov@gmail.com>
2024-09-24 00:40:17 +0300

    Улучшение файловой структуры проекта

---
+++ ImageFeed/Helpers/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Илья Кузнецов on 19.09.2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "ej-XzwMrTTUmvG9_5tdREQjSpZq5FZiMi9SEz2_BOQ4"
+    static let secretKey = "g02jJ4bZGXYIb6qX0YUo-EqITnAh417zE5o4gdeBRG0"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com/")
+}
