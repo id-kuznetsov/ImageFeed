124dbe2d118030e26d039859a5247c2bd83360b5
id-kuznetsov <ilua.kuznetsov@gmail.com>
2024-10-06 19:51:21 +0300

    Устранена гонка блокировкой UI

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -53,11 +53,11 @@ final class AuthViewController: UIViewController {
 
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
-        ProgressHUD.animate()
+        UIBlockingProgressHUD.show()
         oauth2Service.fetchOAuthToken(code: code) { [weak self] result in
             guard let self else { return }
             
-            ProgressHUD.dismiss()
+            UIBlockingProgressHUD.dismiss()
             
             switch result {
             case .success(_):
---
+++ ImageFeed/Helpers/UIBlockingProgressHUD.swift
@@ -0,0 +1,25 @@
+//
+//  UIBlockingProgressHUD.swift
+//  ImageFeed
+//
+//  Created by Ilya Kuznetsov on 06.10.2024.
+//
+
+import UIKit
+import ProgressHUD
+
+class UIBlockingProgressHUD {
+    private static var window: UIWindow? {
+        return UIApplication.shared.windows.first
+    }
+    
+    static func show() {
+        window?.isUserInteractionEnabled = false
+        ProgressHUD.animate()
+    }
+    
+    static func dismiss() {
+        window?.isUserInteractionEnabled = true
+        ProgressHUD.dismiss()
+    }
+}
